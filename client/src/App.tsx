5050ad83402df0be4abd5672a383d62e5b935bb7
Eric Chen <echen9870@gmail.com>
2024-04-06 17:00:33 -0400

    Added main and history conditional rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,37 @@
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Main } from "./components/Main";
+import {History}  from "./components/History";
 function App() {
+  const [page, setPage] = useState("History");
+
+
+
   return (
     <>
-      <div id="header">
-        <ul>
-        <button className="button">Main</button>
-        <button className="button">History</button>
-        <button id="plus" className="button">+</button>
-        </ul>
+      <div id="header" style={styles.navBar}>
+        <button className="button" onClick={ () => {setPage("Main")}}>
+          Main
+        </button>
+        <button className="button" onClick={() => {setPage("History")}}>
+          History
+        </button>
+        <button id="plus" className="button">
+          +
+        </button>
       </div>
-      <div id="appBody">
+      {page === "Main" && <Main></Main>}
+      {page === "History" && <History></History>}
+      
 
-      </div>
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
+
+const styles = {
+  navBar: {
+    textAlign: "right" as const,
+  },
+};
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <>
+    <div>Main</div>
+    <button></button>
+    </>
+    
+  )
+}
+
+export default Main;
